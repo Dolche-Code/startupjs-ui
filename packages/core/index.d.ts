798621283bddc0a61cb2f6ca432e2a8b438e0a1a
Pavel Zhukov <cray0000@gmail.com>
2025-12-03 19:16:33 +0200

    chore: add types to core and update startupjs which has better types

---
+++ packages/core/index.d.ts
@@ -0,0 +1,36 @@
+import type * as React from 'react'
+
+export function u (value?: number): number
+
+export function colorToRGBA (color: string, alpha?: number): string
+
+export interface GetCssVariableOptions {
+  convertToString?: boolean
+}
+
+export type CssVariableValue = string | undefined | Record<string, any>
+
+export function getCssVariable (
+  cssVarName: string,
+  options?: GetCssVariableOptions
+): CssVariableValue
+
+export const Colors: Record<string, string>
+
+export interface GetColorOptions {
+  prefix?: string
+  convertToString?: boolean
+}
+
+export type ColorValue = string | undefined | Record<string, any>
+
+export type GetColor = (color?: string, options?: GetColorOptions) => ColorValue
+
+export function useColors (): GetColor
+
+export interface ThemedOptions {
+  name?: string
+}
+
+export function themed<P> (name: string, component: React.ComponentType<P>): React.ComponentType<P>
+export function themed<P> (component: React.ComponentType<P>): React.ComponentType<P>
