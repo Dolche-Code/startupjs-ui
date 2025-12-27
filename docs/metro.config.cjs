6ce551ff8cac36003fb542e72d69da7bf5590267
Pavel Zhukov <cray0000@gmail.com>
2025-12-27 20:41:49 +0200

    chore: add 'build-static' command to build with expo web in 'static' mode. Use it in github action

--- docs/metro.config.cjs
+++ docs/metro.config.cjs
@@ -1,5 +1,17 @@
 const { getDefaultConfig } = require('startupjs/metro-config')
 
+const DISABLE_MINIFIER = process.env.DISABLE_MINIFIER ?? false
+
 const config = getDefaultConfig(__dirname)
+
+if (DISABLE_MINIFIER) {
+  config.transformer = {
+    ...config.transformer,
+    minifierConfig: {
+      compress: false,
+      mangle: false
+    }
+  }
+}
 
 module.exports = config
