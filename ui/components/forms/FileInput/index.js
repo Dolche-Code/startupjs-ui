863ef26ee8866f3b7501827a6a46e6af8b8a0d23
Pavel Zhukov <cray0000@gmail.com>
2026-03-20 19:34:31 +0200

    chore: update to latest startupjs, remove the old ui library reference code

--- ui/components/forms/FileInput/index.js
+++
@@ -1,4 +0,0 @@
-// the actual component code is intentionally moved out to input.js/input.expo.js
-// so that it can be platform-specific (startupjs adds a priority extension .expo.js
-// which will be loaded first by the bundler if the project is on Expo).
-export { default } from './input'
--- ui/components/toast/helpers.js
+++
@@ -1,3 +0,0 @@
-import { $ } from 'startupjs'
-
-export const $toasts = $()
